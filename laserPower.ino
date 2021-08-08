85ac085708121f5b7e6e2b956cb253262b9d50f6
Jessica Mulein <jessica@mulein.com>
2021-08-08 09:35:56 -0700

    keypad version

--- laserPower.ino
+++ laserPower.ino
@@ -1,72 +1,155 @@
-#include <SparkFun_Qwiic_Twist_Arduino_Library.h>
+#include <Wire.h>
+#include <SerLCD.h>
+#include <SparkFun_Qwiic_Keypad_Arduino_Library.h>
 
 const byte RXLED = 17;  // The RX LED has a defined Arduino pin
 const byte analogPin = 5;   // laser connected to analog pin 3
-const byte twistInterruptPin = 7; //Can be any GPIO
-TWIST twist; //Create instance of this object
-bool powerOn = false;
-byte powerLevel = 0;
-volatile bool interruptOccurred = false;
-bool lastPressed = false;
+const byte keypadInterruptPin = 7;
 
-void TwistEvent()
+volatile bool interruptOccurred = false;
+
+const int keypadBufferLength = 20;
+char keypadBuffer[keypadBufferLength];
+int keypadBufferPos = 0;
+const char PIN[] = "1234";
+
+bool powerOn = false;
+int powerLevel = 0;
+bool pinEntered = false;
+
+KEYPAD keypad;
+SerLCD lcd; // Initialize the library with default I2C address 0x72
+
+void KeypadEvent()
 {
-  interruptOccurred = true;;
+  interruptOccurred = true;
 }
 
 void setup() {
   pinMode(RXLED, OUTPUT);  // sets the pin as output
   pinMode(analogPin, OUTPUT);
-  pinMode(twistInterruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(twistInterruptPin), TwistEvent, FALLING);
-  Wire.begin();
-  if (twist.begin() == false)
-  {
-    Serial.println("Twist does not appear to be connected. Please check wiring. Freezing...");
-    while (1)
-    {
-        digitalWrite(RXLED, powerOn);        
-        delay(500);
-        powerOn = !powerOn;
-    }
-  }
-
+  pinMode(keypadInterruptPin, INPUT); // Qwiic Keypad holds INT pin HIGH @ 3.3V, then LOW when fired.
+  // Note, this means we do not want INPUT_PULLUP.
+  
   // ASAP
   setLaserPower(false, 0);
 
-  //Optional: You can modify the time between when the user has stopped turning and when interrupt is raised
-  twist.setIntTimeout(100); //Set twist timeout to 500ms before interrupt assertion 
-  twist.setLimit(255);
-  twist.setColor(0, 0, 0); //Set Red and Blue LED brightnesses to half of max.
-  twist.connectRed(0);
-  twist.connectGreen(0);
-  twist.connectBlue(10); //Blue LED will go up 10 in brightness with each encoder tick
+  attachInterrupt(digitalPinToInterrupt(keypadInterruptPin), KeypadEvent, FALLING);
+  Wire.begin();
+  Wire.setClock(400000); //Optional - set I2C SCL to High Speed Mode of 400kHz
+  lcd.begin(Wire);
+  lcd.setBacklight(0, 0, 255);
+  lcd.clear(); //Clear the display - this moves the cursor to home position as well
+  lcd.println("Enter PIN");
+  memset(keypadBuffer, 0, keypadBufferLength);
+
+  if (keypad.begin() == false)   // Note, using begin() like this will use default I2C address, 0x4B.
+    // You can pass begin() a different address like so: keypad1.begin(Wire, 0x4A).
+  {
+    Serial.println("Keypad does not appear to be connected. Please check wiring. Freezing...");
+    lcd.println("Keypad missing");
+    while (1);
+  }
 }
 
-void setLaserPower(bool on, byte level)
+void setLaserPower(bool on, int level)
 {
   Serial.println(on ? "POWER ON" : "POWER OFF");
   Serial.println(level);
+  lcd.clear();
+  lcd.println(on ? "POWER ON" : "POWER OFF");
+  lcd.println(level);
   digitalWrite(RXLED, on ? 0 : 1); // inverted
   if (!on) {
     digitalWrite(analogPin, false);
   } else {
-    TXLED1;
     analogWrite(analogPin, level);
   }
   powerOn = on;
   powerLevel = level;
 }
 
+void readCharToBuffer(char c)
+{
+  if (keypadBufferPos == (keypadBufferLength - 2)) {
+    // move down;
+    for (int i = keypadBufferPos; i >= 1; i--) {
+      keypadBuffer[i - 1] = keypadBuffer[i];
+    }
+  }
+  keypadBuffer[keypadBufferPos] = c;
+  keypadBuffer[keypadBufferPos + 1] = 0;
+  if (keypadBufferPos < (keypadBufferLength - 1)) {
+    keypadBufferPos++;
+  }
+}
+
+bool checkPIN(char digit)
+{
+  if (pinEntered) {
+    return true;
+  }
+
+  readCharToBuffer(digit);
+  if (strcmp(keypadBuffer, PIN) == 0) {
+    keypadBufferPos = 0;
+    memset(keypadBuffer, 0, keypadBufferLength);
+    return true;
+  }
+
+  return false;
+}
 
 void loop()
 {
- if (interruptOccurred || twist.isMoved() || (twist.isPressed() != lastPressed))
+  char button = char(keypad.getButton());
+
+  if (interruptOccurred || (button > 0))
   {
     interruptOccurred = false;
-    bool pressed = twist.isPressed();
-    setLaserPower(pressed, twist.getCount());
-    lastPressed = pressed;
-    twist.clearInterrupts(); //Clear any interrupt bits
+
+    keypad.updateFIFO();  // necessary for keypad to pull button from stack to readable register
+
+    if (button == -1)
+    {
+      Serial.println("No keypad detected");    
+    }
+    else if (button != 0)
+    {    
+      if (!pinEntered) {
+        if (button == '#') {
+          lcd.clear();
+          keypadBufferPos = 0;
+          memset(keypadBuffer, 0, keypadBufferLength);
+        } else {
+          lcd.write('*');
+          if (checkPIN(button)) {
+            pinEntered = true;
+            lcd.clear();
+            lcd.println("Laser Enabled");
+          }
+        }
+
+      } else if (pinEntered && (keypadBufferPos == 0) && (powerLevel < 255) && (button == '#')) {
+        setLaserPower(powerOn, powerLevel + 1);
+    
+      } else if (pinEntered && (keypadBufferPos == 0) && (powerLevel > 0) && (button == '*')) {
+        setLaserPower(powerOn, powerLevel - 1);
+
+      } else if (button == '#') {
+        // try to adjust power
+        int testPower = atoi(keypadBuffer);
+        if (testPower >= 0 && testPower <= 255) {
+          setLaserPower(powerOn, testPower);
+        }
+        keypadBufferPos = 0;
+        memset(keypadBuffer, 0, keypadBufferLength);
+
+      } else {
+        readCharToBuffer(button);
+        lcd.write(button);
+
+      }
+    }
   }
 }
